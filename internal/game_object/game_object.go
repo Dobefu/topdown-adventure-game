88f153fdf6512c57d89ce4d3de636010b5ad99d6
Connor van Spronssen <info@dobefu.com>
2025-06-22 11:11:19 +0200

    feat: Add placeholder methods to the GameObject struct

--- internal/game_object/game_object.go
+++ internal/game_object/game_object.go
@@ -3,6 +3,7 @@ package game_object
 import (
 	"github.com/Dobefu/topdown-adventure-game/internal/interfaces"
 	"github.com/Dobefu/vectors"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type GameObject struct {
@@ -10,4 +11,12 @@ type GameObject struct {
 
 	position vectors.Vector3
 	isActive bool
+}
+
+func (g *GameObject) Draw(screen *ebiten.Image) {
+	// noop
+}
+
+func (g *GameObject) Update() (err error) {
+	return nil
 }
