6121e6aa7effb8977a0e427512f7ac8c0a5c25b2
Connor van Spronssen <info@dobefu.com>
2025-06-22 12:30:30 +0200

    feat: Add a button with default options

---
+++ internal/ui/button.go
@@ -0,0 +1,66 @@
+package ui
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/Dobefu/topdown-adventure-game/internal/fonts"
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
+)
+
+var (
+	buttonImage *widget.ButtonImage
+)
+
+func init() {
+	var err error
+	buttonImage, err = loadButtonImage()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewButton(opts ...widget.ButtonOpt) *widget.Button {
+	defaultOpts := []widget.ButtonOpt{
+		widget.ButtonOpts.TextPadding(widget.Insets{
+			Top:    12,
+			Left:   12,
+			Right:  12,
+			Bottom: 12,
+		}),
+		widget.ButtonOpts.TextFace(fonts.FontDefaultMd),
+		widget.ButtonOpts.TextColor(&widget.ButtonTextColor{
+			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
+		}),
+		widget.ButtonOpts.Image(buttonImage),
+	}
+
+	return widget.NewButton(append(defaultOpts, opts...)...)
+}
+
+func loadButtonImage() (*widget.ButtonImage, error) {
+	idle := image.NewBorderedNineSliceColor(
+		color.NRGBA{R: 170, G: 170, B: 180, A: 255},
+		color.NRGBA{90, 90, 90, 255},
+		3,
+	)
+
+	hover := image.NewBorderedNineSliceColor(
+		color.NRGBA{R: 130, G: 130, B: 150, A: 255},
+		color.NRGBA{70, 70, 70, 255},
+		3,
+	)
+
+	pressed := image.NewAdvancedNineSliceColor(
+		color.NRGBA{R: 130, G: 130, B: 150, A: 255},
+		image.NewBorder(3, 2, 2, 2, color.NRGBA{70, 70, 70, 255}),
+	)
+
+	return &widget.ButtonImage{
+		Idle:    idle,
+		Hover:   hover,
+		Pressed: pressed,
+	}, nil
+}
