d5a3337a23529226b05b66f3f9a27af9320eb5f9
Connor van Spronssen <info@dobefu.com>
2025-06-22 11:12:28 +0200

    feat: Add logic to process active gameobjects in a scene

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,32 +1,77 @@
 package game
 
 import (
+	"github.com/Dobefu/topdown-adventure-game/internal/interfaces"
+	"github.com/Dobefu/topdown-adventure-game/internal/scene"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/yohamta/donburi"
 )
 
 type Game interface {
+	ebiten.Game
+
+	GetScene() (scene interfaces.Scene)
+	SetScene(scene interfaces.Scene)
 }
 
 type game struct {
 	Game
 
-	world donburi.World
+	scene interfaces.Scene
 }
 
-func NewGame() *game {
-	return &game{
-		world: donburi.NewWorld(),
+func NewGame() (g *game) {
+	g = &game{}
+	g.SetScene(&scene.Level1Scene{})
+
+	return g
+}
+
+func (g *game) SetScene(scene interfaces.Scene) {
+	g.scene = scene
+
+	g.scene.Init()
+}
+
+func (g *game) Update() (err error) {
+	if g.scene == nil {
+		return nil
 	}
-}
 
-func (g *game) Update() error {
+	gameObjects := g.scene.GetGameObjects()
+
+	for _, gameObject := range gameObjects {
+		if !gameObject.GetIsActive() {
+			continue
+		}
+
+		err = gameObject.Update()
+
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, screen.Bounds().Max.String())
+	if g.scene == nil {
+		return
+	}
+
+	gameObjects := g.scene.GetGameObjects()
+
+	for _, gameObject := range gameObjects {
+		if !gameObject.GetIsActive() {
+			continue
+		}
+
+		gameObject.Draw(screen)
+	}
+}
+
+func (g *game) DrawFinalScreen(screen ebiten.FinalScreen, offscreen *ebiten.Image, geoM ebiten.GeoM) {
+	screen.DrawImage(offscreen, nil)
 }
 
 func (g *game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
