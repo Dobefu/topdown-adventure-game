896c4e3ba92203a56c946de4744352ab78d2bd4d
Connor van Spronssen <info@dobefu.com>
2025-06-23 08:26:28 +0200

    feat: Set a default window size on desktop

--- main.go
+++ main.go
@@ -9,10 +9,9 @@ import (
 
 func main() {
 	ebiten.SetWindowTitle("Top-down Adventure Game")
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeDisabled)
+	ebiten.SetWindowSize(1280, 720)
 
-	// Set a minimum size limit.
-	ebiten.SetWindowSizeLimits(320, 320, int(^uint16(0)), int(^uint16(0)))
 	ebiten.SetScreenClearedEveryFrame(false)
 
 	gameOptions := ebiten.RunGameOptions{
