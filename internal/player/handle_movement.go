281f2c8009c6e9a0b1a20b5edb4c73f852e83d0d
Connor van Spronssen <info@dobefu.com>
2025-06-24 07:26:39 +0200

    chore: Make the player move a bit quicker

--- internal/player/handle_movement.go
+++ internal/player/handle_movement.go
@@ -11,24 +11,24 @@ func (p *Player) handleMovement() {
 	// Dampen the X and Y velocity.
 	p.velocity.Mul(vectors.Vector3{X: .9, Y: .9, Z: 1})
 
-	if math.Abs(p.velocity.X) < .2 && math.Abs(p.velocity.Y) < .2 {
+	if math.Abs(p.velocity.X) < .25 && math.Abs(p.velocity.Y) < .25 {
 		p.velocity.Mul(vectors.Vector3{X: 0, Y: 0, Z: 1})
 	}
 
 	if p.input.ActionIsPressed(input.ActionMoveLeft) {
-		p.velocity.X -= .25
+		p.velocity.X -= .3
 	}
 
 	if p.input.ActionIsPressed(input.ActionMoveRight) {
-		p.velocity.X += .25
+		p.velocity.X += .3
 	}
 
 	if p.input.ActionIsPressed(input.ActionMoveUp) {
-		p.velocity.Y -= .25
+		p.velocity.Y -= .3
 	}
 
 	if p.input.ActionIsPressed(input.ActionMoveDown) {
-		p.velocity.Y += .25
+		p.velocity.Y += .3
 	}
 
 	pos := p.GetPosition()
