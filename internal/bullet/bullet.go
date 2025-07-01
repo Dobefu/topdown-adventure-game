7f9c6fa5b37f09db0aff210634c4235f8c83e1f7
Connor van Spronssen <info@dobefu.com>
2025-07-01 08:14:52 +0200

    feat: Add a bullet gameObject

---
+++ internal/bullet/bullet.go
@@ -0,0 +1,126 @@
+package bullet
+
+import (
+	"bytes"
+	_ "embed"
+	"image"
+	"log"
+	"math"
+
+	"github.com/Dobefu/topdown-adventure-game/internal/animation"
+	"github.com/Dobefu/topdown-adventure-game/internal/game_object"
+	"github.com/Dobefu/topdown-adventure-game/internal/interfaces"
+	"github.com/Dobefu/vectors"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+var (
+	//go:embed img/bullet.png
+	bulletImgBytes []byte
+	bulletImg      *ebiten.Image
+	bulletSubImgs  []*ebiten.Image
+)
+
+const (
+	FRAME_WIDTH  = 32
+	FRAME_HEIGHT = 32
+	NUM_FRAMES   = 16
+)
+
+func init() {
+	img, _, err := image.Decode(bytes.NewReader(bulletImgBytes))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bulletImg = ebiten.NewImageFromImage(img)
+	bulletSubImgs = make(
+		[]*ebiten.Image,
+		bulletImg.Bounds().Dy()/FRAME_HEIGHT*NUM_FRAMES,
+	)
+
+	for state := range bulletImg.Bounds().Dy() / FRAME_HEIGHT {
+		for frame := range NUM_FRAMES {
+			key := state*NUM_FRAMES + frame
+
+			bulletSubImgs[key] = ebiten.NewImageFromImage(
+				bulletImg.SubImage(
+					image.Rect(
+						frame*FRAME_WIDTH,
+						state*FRAME_HEIGHT,
+						frame*FRAME_WIDTH+FRAME_WIDTH,
+						state*FRAME_HEIGHT+FRAME_HEIGHT,
+					),
+				),
+			)
+		}
+	}
+}
+
+type Bullet struct {
+	interfaces.Bullet
+	game_object.GameObject
+
+	velocity vectors.Vector3
+
+	imgOptions     *ebiten.DrawImageOptions
+	frameIndex     int
+	animationState animation.AnimationState
+}
+
+func NewBullet() (bullet *Bullet) {
+	bullet = &Bullet{
+		velocity: vectors.Vector3{},
+	}
+
+	bullet.imgOptions = &ebiten.DrawImageOptions{}
+
+	bullet.SetIsActive(false)
+
+	return bullet
+}
+
+func (b *Bullet) Fire(
+	from vectors.Vector3,
+	angle float64,
+	velocity vectors.Vector3,
+) {
+	b.SetIsActive(true)
+	b.SetPosition(from)
+
+	velocity.Add(vectors.Vector3{
+		X: -math.Cos(angle+math.Pi/2) * 15,
+		Y: -math.Sin(angle+math.Pi/2) * 15,
+		Z: 0,
+	})
+
+	b.velocity = velocity
+}
+
+func (b *Bullet) SetVelocity(velocity vectors.Vector3) {
+	b.velocity = velocity
+}
+
+func (b *Bullet) Draw(screen *ebiten.Image) {
+	pos := b.GetPosition()
+
+	scene := (*b.GetScene())
+	camera := scene.GetCamera()
+
+	b.imgOptions.GeoM.Reset()
+	b.imgOptions.GeoM.Translate(pos.X, pos.Y)
+
+	camera.Draw(
+		bulletSubImgs[int(b.animationState)*NUM_FRAMES+b.frameIndex],
+		b.imgOptions,
+		screen,
+	)
+}
+
+func (b *Bullet) Update() (err error) {
+	pos := b.GetPosition()
+	pos.Add(b.velocity)
+
+	return nil
+}
