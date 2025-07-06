d37c910b7007f5c29e533fcbf785a2eb09200c58
Connor van Spronssen <info@dobefu.com>
2025-07-06 11:21:02 +0200

    feat: Add an enemy to the overworld

--- internal/scene/overworld_scene.go
+++ internal/scene/overworld_scene.go
@@ -1,6 +1,7 @@
 package scene
 
 import (
+	"github.com/Dobefu/topdown-adventure-game/internal/enemy"
 	"github.com/Dobefu/topdown-adventure-game/internal/player"
 	"github.com/Dobefu/vectors"
 )
@@ -14,9 +15,11 @@ func (s *OverworldScene) Init() {
 	s.InitSceneMap("maps/overworld.tmx")
 
 	player := player.NewPlayer(vectors.Vector3{X: 0, Y: 0, Z: 0})
-
 	s.AddGameObject(player)
 	s.SetCameraTarget(player)
+
+	enemy := enemy.NewEnemy(vectors.Vector3{X: 160, Y: 240, Z: 0})
+	s.AddGameObject(enemy)
 }
 
 func (s *OverworldScene) InitUI() {
