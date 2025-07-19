0d4740536066f0e68eee904acc754b74607f2497
Connor van Spronssen <info@dobefu.com>
2025-07-19 13:08:55 +0200

    feat: Add a pixel particle

---
+++ internal/particles/pixel/pixel.go
@@ -0,0 +1,48 @@
+package pixel
+
+import (
+	"image/color"
+
+	"github.com/Dobefu/topdown-adventure-game/internal/particles"
+	"github.com/Dobefu/vectors"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Pixel struct {
+	particles.Particle
+
+	imgOptions *ebiten.DrawImageOptions
+	img        *ebiten.Image
+}
+
+func NewPixel(position vectors.Vector3) (pixel *Pixel) {
+	pixel = &Pixel{}
+	pixel.SetPosition(position)
+
+	pixel.imgOptions = &ebiten.DrawImageOptions{}
+	pixel.img = ebiten.NewImage(1, 1)
+	pixel.img.Fill(color.White)
+
+	return pixel
+}
+
+func (p *Pixel) Update() (err error) {
+	return nil
+}
+
+func (p *Pixel) Draw(screen *ebiten.Image) {
+	p.Particle.Update()
+
+	scene := *p.GetScene()
+	camera := scene.GetCamera()
+	position := p.GetPosition()
+
+	p.imgOptions.GeoM.Reset()
+	p.imgOptions.GeoM.Translate(position.X, position.Y)
+
+	camera.Draw(
+		p.img,
+		p.imgOptions,
+		screen,
+	)
+}
