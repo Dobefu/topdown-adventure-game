1a80152e8b2c555416a2b99e68f7b4380dec6548
Connor van Spronssen <info@dobefu.com>
2025-07-20 14:16:10 +0200

    chore: Add doc comments to the state package

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,8 +1,12 @@
+// Package state provides constants for generic states.
 package state
 
+// State houses the different state constants.
 type State byte
 
 const (
+	// StateDefault is the default state.
 	StateDefault = iota
+	// StateHurt is the hurt state.
 	StateHurt
 )
