e4c77328c5a5069ef756291776f577ec830fbcb8
Connor van Spronssen <info@dobefu.com>
2025-07-22 07:12:59 +0200

    chore: Add constants for the collision tiles

---
+++ internal/tiledata/tiledata.go
@@ -0,0 +1,11 @@
+// Package tiledata houses several constants that identify tiles by their ID.
+package tiledata
+
+const (
+	// TileCollisionWall collision tile.
+	TileCollisionWall = iota + 257
+	// TileCollisionLedge collision tile.
+	TileCollisionLedge
+	// TileCollisionWater collision tile.
+	TileCollisionWater
+)
