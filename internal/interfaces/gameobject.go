737db7779fd4cefc389fe74c4f2115eeb1a9f55c
Connor van Spronssen <info@dobefu.com>
2025-07-27 16:09:05 +0200

    chore: Remove some redundant methods from the gameobject interface

--- internal/interfaces/gameobject.go
+++ internal/interfaces/gameobject.go
@@ -14,12 +14,8 @@ type GameObject interface {
 	DrawAbove(screen *ebiten.Image)
 	DrawUI(screen *ebiten.Image)
 	GetID() (id uint64)
-	SetID(id uint64)
 	GetPosition() (position *vectors.Vector3)
-	SetPosition(position vectors.Vector3)
 	GetCameraPosition() (position *vectors.Vector3)
-	SetIsActive(isActive bool)
 	GetIsActive() (isActive bool)
 	SetScene(scene Scene)
-	GetScene() *Scene
 }
